d1a1d2c80623aa8d39a8794d3ccdde32ac1edf73
Rebecca Xu <rebeccaxu@Rebeccas-MacBook-Pro.local>
2018-04-18 20:48:41 -0700

    feedcard

--- src/components/FeedCard.js
+++ src/components/FeedCard.js
@@ -1,27 +1,56 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../sass/feedcard.scss';
 
 const SampleContent = {
-  category: "image",
-  image: "https://placeimg.com/1024/768/nature",
+  category: 'Music',
+  image: 'https://placeimg.com/1024/768/nature',
   text:
-    "Sed at blandit diam. Cras accumsan in ligula sit amet malesuada. Praesent nec odio dapibus, auctor erat ac,\n facilisis risus. In hac habitasse platea dictumst. Etiam sit amet tristique elit,\n sit amet maximus sem. Praesent pharetra safddsf nibh eu tincidunt. Sed dapibus tempus luctus.\n Proin sit amet diam cursus, eleifend sapien eu, viverra sem",
-  time: "9:00",
-  title: "Hello World"
+    'Sed at blandit diam. Cras accumsan in ligula sit amet malesuada. Praesent nec odio dapibus, auctor erat ac,\n facilisis risus. In hac habitasse platea dictumst. Etiam sit amet tristique elit,\n sit amet maximus sem. Praesent pharetra safddsf nibh eu tincidunt. Sed dapibus tempus luctus.\n Proin sit amet diam cursus, eleifend sapien eu, viverra sem',
+  time: '9:00',
+  title: 'Hello World',
 };
 
 export default class FeedCard extends React.Component {
   constructor(props) {
     super(props);
-    // props should contain a property 'member'
-    /* member has the structure
+    console.log(this.props);
+  }
+  // props should contain a property 'content'
+  /* content has the structure
       {
         category: "image"
         image:
       }
     */
-  }
 
   render() {
-    return <div />;
+    return (
+      <div className="FeedCard container">
+        <div className={'heading row no-gutters ' + this.props.dayClass}>
+          <div className="category col text-left">
+            <h2>{this.props.content.category}</h2>
+          </div>
+          <div className="time col text-right">
+            <h2>{this.props.content.time}</h2>
+          </div>
+        </div>
+        <div className={'content ' + this.props.dayClass}>
+          <div className="title row">
+            <h2>{this.props.content.title}</h2>
+          </div>
+          <div className="image row">
+            <img
+              src={this.props.content.image}
+              class="img-fluid"
+              alt="Responsive image"
+            />
+          </div>
+          <div className="text row">
+            <h3>{this.props.content.text}</h3>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import testImg from '../assets/grounds.jpg';
+import React from "react";
+import { Link } from "react-router-dom";
+import testImg from "../assets/grounds.jpg";
 
 export default class LandingPage extends React.Component {
   render() {
