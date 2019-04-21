4d6cf39460e740c3ac10381ae015d7fae0fb6c80
Michael <myque@live.com>
2019-04-20 17:08:40 -0700

    fix mobile multi images

--- src/components/FeedCard.js
+++ src/components/FeedCard.js
@@ -6,6 +6,22 @@ export default class FeedCard extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  componentDidMount() {
+    const { hash } = window.location;
+    if (hash !== '') {
+    // Push onto callback queue so it runs after the DOM is updated,
+    // this is required when navigating from a different page so that
+    // the element is rendered on the page before trying to getElementById.
+    setTimeout(() => {
+      const id = hash.replace('#', '');
+      const slug = id.slice(id.lastIndexOf("#") + 1, id.length)
+      console.log(slug);
+      const element = document.getElementById(slug);
+      if (element) element.scrollIntoView();
+    }, 0);
+  }
+  }
   // props should contain a property 'content'
   /* content has the structure
       {
@@ -16,7 +32,7 @@ export default class FeedCard extends React.Component {
 
   render() {
     return (
-      <div className="FeedCard">
+      <div className="FeedCard" id={this.props.content.slug}>
         <div className={"heading row no-gutters "}>
           <div className="category col text-left">
             <h2>{this.props.content.day}</h2>
